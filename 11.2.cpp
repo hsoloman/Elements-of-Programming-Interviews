ca6f73f8d08f4b29d0321b7c5c3abce2f6c967cf
hsoloman <hsoloman@gmail.com>
2014-02-16 15:38:23 -0500

    Create 11.2.cpp

---
+++ 11.2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// returns index
+template <typename T>
+int firstOccuranceLargerThanK(const int k, const vector<T>& A){
+    int found(-1);
+    int left(0), right(A.size() -1);
+    
+    while(left <= right){
+        int mid = left + (right - left)/2;
+        
+        if (A[mid] <= k){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+            found = (found == -1 || mid < found) ? mid : found;
+        }
+        
+    }
+
+    return found;
+} 
+
+//###########################################################################
+int main()
+{
+    vector<int> A = {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    cout<< "first occurance larger than -14: \tA[" << firstOccuranceLargerThanK(-14, A) << "]" << endl;
+    cout<< "first occurance larger than 108: \tA[" << firstOccuranceLargerThanK(108, A) << "]" << endl;
+    cout<< "first occurance larger than 285: \tA[" << firstOccuranceLargerThanK(285, A) << "]" << endl;
+    cout<< "first occurance larger than 2: \t\tA[" << firstOccuranceLargerThanK(2, A) << "]" << endl;
+    cout<< "first occurance larger than 500: \tA[" << firstOccuranceLargerThanK(500, A) << "]" << endl;
+
+	return 0;
+}
