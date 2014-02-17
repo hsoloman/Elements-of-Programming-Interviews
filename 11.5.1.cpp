855d29c466e35fb593f11cec80643b500e531ae4
hsoloman <hsoloman@gmail.com>
2014-02-17 18:35:42 -0500

    Update 11.5.1.cpp
    while (left <= right)... fixes edge case for A[0]

--- 11.5.1.cpp
+++ 11.5.1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int indexOfK(const vector<int>& A, int k){
     int left(0), right(A.size() -1);
     
-    while(left < right) {
+    while(left <= right) {
         int mid = left + (right-left)/2;
         
         cout << mid << endl;
@@ -18,16 +18,16 @@ int indexOfK(const vector<int>& A, int k){
         if (A[mid] < A[right]){ // right half contiguous
         
             if (k >= A[mid] && k <= A[right]){  // k in right half
-                left = mid;
+                left = mid + 1;
             } else {
-                right = mid -1;
+                right = mid - 1;
             }
             
         } else {                // left half contiguous
             if (k >= A[left] && k <= A[mid]){
-                right = mid;
+                right = mid + 1;
             } else {
-                right = mid -1;
+                right = mid - 1;
             }
         }
     }
@@ -40,7 +40,7 @@ int main()
     vector<int> A = {378, 478, 550, 631, 103, 203, 220, 234, 279, 368};
     // min is at A[4]
         
-    int k = 550;
+    int k = 368;
     int index = indexOfK(A, k);
     cout << k << "  found at A[" << index << "]" << endl;
     return 0;
