bce193bc5d2bd7b7b57b63841c03adca1ccb3f98
hsoloman <hsoloman@gmail.com>
2014-02-17 18:31:10 -0500

    Create 11.5.1.cpp

---
+++ 11.5.1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// returns index of minimum element in a cyclically sorted array
+// returns -1 if not found
+int indexOfK(const vector<int>& A, int k){
+    int left(0), right(A.size() -1);
+    
+    while(left < right) {
+        int mid = left + (right-left)/2;
+        
+        cout << mid << endl;
+        
+        if (A[mid] == k) return mid;
+        
+        if (A[mid] < A[right]){ // right half contiguous
+        
+            if (k >= A[mid] && k <= A[right]){  // k in right half
+                left = mid;
+            } else {
+                right = mid -1;
+            }
+            
+        } else {                // left half contiguous
+            if (k >= A[left] && k <= A[mid]){
+                right = mid;
+            } else {
+                right = mid -1;
+            }
+        }
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    vector<int> A = {378, 478, 550, 631, 103, 203, 220, 234, 279, 368};
+    // min is at A[4]
+        
+    int k = 550;
+    int index = indexOfK(A, k);
+    cout << k << "  found at A[" << index << "]" << endl;
+    return 0;
+}
